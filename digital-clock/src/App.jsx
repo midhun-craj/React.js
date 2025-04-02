893841abba3becd4fe03712db711ab74f987004b
midhun-craj <midhuncraj.m@gmail.com>
2025-04-03 00:58:13 +0530

    Done Digital clock app

---
+++ digital-clock/src/App.jsx
@@ -0,0 +1,9 @@
+import DigitalClock from "./DigitalClock/DigitalClock.jsx";
+
+export default function App() {
+  return(
+    <>
+      <DigitalClock />
+    </>
+  );
+}
---
+++ digital-clock/src/DigitalClock/DigitalClock.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect} from 'react';
+import styles from './DigitalClock.module.css'
+
+export default function DigitalClock() {
+
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        }, 1000)
+
+        return(() => {
+            clearInterval(intervalId);
+        })
+    });
+
+    function formatTime() {
+        let hours = time.getHours();
+        const minutes = time.getMinutes();
+        const seconds = time.getSeconds();
+        const meridiem = hours > 12 ? "PM" : "AM";
+
+        hours = hours % 12 || 12;
+
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`;
+    }
+
+    function padZero(number) {
+        return (number < 10 ? "0" : "") + number
+    }
+
+    return(
+        <div className={styles['clock-container']}>
+            <video autoPlay loop muted playsInline>
+                <source src='https://cdn.pixabay.com/video/2022/04/16/114055-700074063_large.mp4' />
+                Your browser doesn't support the video.
+            </video>
+            <div className={styles['clock']}>
+                <span>{formatTime()}</span>
+            </div>
+        </div>
+    );
+}
